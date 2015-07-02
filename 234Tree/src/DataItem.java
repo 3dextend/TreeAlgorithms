a5140110168ca882fe304ffdfd3ee1356d7912d6
Ryabenkiy Sergiy <3dextend@gmail.com>
2015-07-03 01:35:27 +0300

    Add 234Tree Algorithm

---
+++ 234Tree/src/DataItem.java
@@ -0,0 +1,17 @@
+/**
+ * 234TreeAlgorithm
+ * Created by Ryabenkiy Sergey on 03.07.2015 1:21.
+ */
+public class DataItem {
+    public long dData; // Один объект данных
+    //--------------------------------------------------------------
+    public DataItem(long dd) // Конструктор
+    { dData = dd; }
+    //--------------------------------------------------------------
+    public void displayItem() // Вывод элемента в формате "/27"
+    { System.out.print("/"+dData); }
+//--------------------------------------------------------------
+} // Конец класса DataItem
+
+
+
